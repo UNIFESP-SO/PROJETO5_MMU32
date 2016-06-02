9921ac3c8ef3285d5c7fc4ec40731696f96d8e15
FernandoSoares <sax.soares@gmail.com>
2016-06-02 20:19:57 -0300

    Add files via upload

--- MMU_32.c
+++ MMU_32.c
@@ -4,6 +4,9 @@
 #include <string.h>
 #include <inttypes.h>
 
+#define TRUE 1
+#define FALSE 0
+
 struct virtual_page_t{
     uint32_t cache:1;
     uint32_t ref:1;
@@ -50,10 +53,46 @@ int get_frame_addr(uint32_t lvaddr, uint32_t *faddr, vtab_t virtual_tab[][VTAB_L
     return R_OK;
 }
 
+struct age_t {
+    uint8_t age;
+    uint8_t alloc;
+    uint16_t l1;    // tab nivel 1
+    uint16_t l2;    // tab nivel 2
+};
+typedef struct age_t age_t;
 
-int main(){
-
+void aging(age_t at[], vtab_t vt[][VTAB_LEN]){
+    int i, j;
+    for(i = 0; i < VTAB_LEN; i++){
+        for(j = 0; j < VTAB_LEN; j++){
+            if(vt[i][j].exist) {
+                int k = vt[i][j].frame;
+                at[k].alloc = TRUE;
+                at[k].age = at[k].age >> 1;
+                at[k].age = at[k].age | (vt[i][j].ref << 7);
+            }
+        }
+    }
 }
+
+#define FRAME_NUM 524288    //  2^19
+// NUR -> NOT UTILIZED RECENTLY
+int get_frame_NUR(age_t at[]){
+    int i, imin = -1;
+    int min = 256;
+
+    for(i = 0; i < FRAME_NUM; i++){
+        if( !at[i].alloc) return i;
+        else{
+            if(at[i].age < min){
+                min = at[i].age;
+                imin = i;
+            }
+        }
+    }
+}
+
+int main(){}
 /*
 pagina_t *get_pagina_virtual(pagina_t memo_virtual[], uint16_t end_virtual){
     pagina_t end;
